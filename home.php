c4a4724d1defb0b2cac182d2fae33e3366667b25
erickkartiadi <erickcartiady@gmail.com>
2019-04-17 19:36:25 +0700

    added carousel

--- home.php
+++ home.php
@@ -8,47 +8,87 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Aphrodite</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        
+    </style>
+    </head>
+
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="#">
-        <img src="img/logo/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        aphrodite
+            <img src="img/logo/logo.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+            aphrodite
         </a>
     </nav>
 
+    <div id="carouselId" class="carousel slide mt-0" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselId" data-slide-to="1"></li>
+                    <li data-target="#carouselId" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" style="height:80vh" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="img-fluid" src="images/carousel/product1.jpg" data-src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-fluid" src="images/carousel/product2.jpg" data-src="holder.js/900x500/auto/#666:#444/text:Second slide" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="img-fluid" src="images/carousel/product3.jpg" data-src="holder.js/900x500/auto/#666:#444/text:Third slide" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
     <!-- Content -->
-    <section class="konten">
+    <section class="konten mt-3">
         <div class="container">
-            <h1>Product</h1>
             <div class="row">
                 <?php
-                $ambil = $koneksi->query("SELECT * FROM produk");
-                while ($perproduk = $ambil->fetch_assoc()){
+                    $ambil = $koneksi->query("SELECT * FROM produk");
+                    while ($perproduk = $ambil->fetch_assoc()){
                 ?>
                 <div class="col-md-3">
                     <div class="card">
                         <img src="foto_produk/<?php echo $perproduk['foto_produk'];?>" alt="" class="card-img-top">
                         <div class="card-body">
                             <h3 class="card-title"><?php echo $perproduk['nama_produk'];?></h3>
-                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, incidunt!</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+                                incidunt!</p>
                             <h5 class="card-subtitle">Rp. <?php echo number_format($perproduk['harga_produk']);?></h5>
                             <br>
                             <a href="beli.php?id=<?php echo $perproduk['id_produk'];?>" class="btn btn-primary">Beli</a>
-                            <a href="detail.php?id=<?php echo $perproduk['id_produk'];?>" class="btn btn-default">Detail</a>
+                            <a href="detail.php?id=<?php echo $perproduk['id_produk'];?>"
+                                class="btn btn-default">Detail</a>
                         </div>
                     </div>
                 </div>
                 <?php 
-                } 
+                    } 
                 ?>
             </div>
         </div>
     </section>
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+
 </html>
