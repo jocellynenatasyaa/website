00a219d0ba336072f0e2c4e0e9ca2641d076cf25
erickkartiadi <erickcartiady@gmail.com>
2019-04-17 21:58:13 +0700

    Navbar and  Carousel fixed

--- home.php
+++ home.php
@@ -9,53 +9,95 @@
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Aphrodite</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <style>
+        .nav-link {
+            color: white;
+        }
 
+        .bg-luxury-blue {
+            background: #002A54;
+        }
+
+        .dropdown-toggle:after {
+            content: none
+        }
     </style>
 </head>
 
-<body>
+<body class="bg-light">
     <!-- Navbar -->
-    <nav class=" navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-luxury-blue text-white">
         <a class="navbar-brand" href="#">
-            <img src="img/logo/logo_white.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-            aphrodite
+            <img src="img/logo/logo_font_white.svg" style="height:23px;" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <form class="form-inline my-2 my-lg-0">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto container-fluid justify-content-between">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Category
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="col-12 col-lg-9">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Search" autocomplete="off">
+                        <input type="text" class="form-control" placeholder="Search...">
                         <div class="input-group-append">
-                            <button class="btn btn-secondary">
+                            <button class="btn btn-info" type="button">
                                 <i class="fa fa-search"></i>
                             </button>
                         </div>
                     </div>
-                </form>
-            </div>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-bell"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Your Name</a>
+                </li>
+
+            </ul>
         </div>
-
-
     </nav>
 
-    <div id="carouselId" class="carousel slide mt-0" data-ride="carousel">
+    <!-- carousel -->
+
+    <div style="margin-top:40px;" id="carouselId" class="carousel slide" data-ride="carousel">
         <ol class="carousel-indicators">
             <li data-target="#carouselId" data-slide-to="0" class="active"></li>
             <li data-target="#carouselId" data-slide-to="1"></li>
             <li data-target="#carouselId" data-slide-to="2"></li>
+            <li data-target="#carouselId" data-slide-to="3"></li>
         </ol>
-        <div class="carousel-inner" style="height:80vh" role="listbox">
+        <div class="carousel-inner" style="height:95vh" role="listbox">
             <div class="carousel-item active">
-                <img class="img-fluid" src="images/carousel/product1.jpg"
+                <img style="margin: auto;" class="img-fluid" src="images/carousel/product1.jpg"
                     data-src="holder.js/900x500/auto/#777:#555/text:First slide" alt="First slide">
             </div>
             <div class="carousel-item">
@@ -64,6 +106,10 @@
             </div>
             <div class="carousel-item">
                 <img class="img-fluid" src="images/carousel/product3.jpg"
+                    data-src="holder.js/900x500/auto/#666:#444/text:Third slide" alt="Third slide">
+            </div>
+            <div class="carousel-item">
+                <img class="img-fluid" src="images/carousel/product4.jpg"
                     data-src="holder.js/900x500/auto/#666:#444/text:Third slide" alt="Third slide">
             </div>
         </div>
