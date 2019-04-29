74b3adbf8cf8878ed4616fe0826f7b72ff9c8670
stevanny11 <stevannyanjelikaaa@gmail.com>
2019-04-29 20:29:45 +0700

    hai nya hilang

--- home.php
+++ home.php
@@ -229,15 +229,6 @@
 
     <!-- flash sale -->
 
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            hai
-        </div>
-        <div class="col-6">
-        </div>
-    </div>
-</div>
 
 
 
