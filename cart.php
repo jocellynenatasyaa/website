8f8bfe4a72144943113f9c26c22a093d531b7b5e
stevanny11 <stevannyanjelikaaa@gmail.com>
2019-04-30 10:44:28 +0700

    ubah navbar

--- cart.php
+++ cart.php
@@ -90,83 +90,83 @@
 </head>
 
 <!-- Navbar -->
-<nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-luxury-blue text-white">
-    <a class="navbar-brand" href="#" onclick="loadLandingPage()">
-        <img src="img/logo/logo_font_white.svg" style="height:23px;" alt="">
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<body class="bg-light">
+    <!-- Navbar -->
+    <nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-luxury-blue text-white">
+        <a class="navbar-brand" href="#">
+            <img src="img/logo/logo_font_white.svg" style="height:23px;" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto container-fluid justify-content-between">
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-                    aria-haspopup="true" aria-expanded="false">
-                    Category
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="col-12 col-lg-9">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search...">
-                    <div class="input-group-append">
-                        <button class="btn btn-info" type="button">
-                            <i class="fa fa-search" onclick="f_search()"></i>
-                        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto container-fluid justify-content-between">
+            <div class="dropdown">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+     Category
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="category_men.html">Men</a>
+      <a class="dropdown-item" href="category_woman.html">Woman</a>
+    </div>
+  </div>
+
+                <li class="col-12 col-lg-9">
+                <form action="search.php" method="GET">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search..." name="search">
+                        <div class="input-group-append">
+                            <button type="button" class="btn btn-primary">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link" onclick="loadCartPage()">
-                    <i class="fas fa-shopping-cart"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="fas fa-bell"></i>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="fas fa-envelope"></i>
-                </a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle text-white text-capitalize text-center" href="#" id="navbarDropdown"
-                    role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-user"></i>
-                    <?php
-                                echo $_SESSION['username'];
-                            ?>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a href="#" class="dropdown-item text-center text-capitalize">
+                </form>
+                </li>
+                <li class="nav-item">
+                    <a href="cart.php" class="nav-link">
+                        <i class="fas fa-shopping-cart"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-bell"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="fas fa-envelope"></i>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white text-capitalize text-center" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-user"></i>
                         <?php
                                 echo $_SESSION['username'];
                             ?>
                     </a>
-                    <div class="dropdown-divider"></div>
-                    <div class="container">
-                        <a class="btn btn-primary col-12" href="login.php">
-                            <i class="fas fa-sign-out-alt"></i>
-                            Logout
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a href="#" class="dropdown-item text-center text-capitalize">
+                        <i class="fas fa-user"></i>
+                            <?php
+                                echo $_SESSION['username'];
+                            ?>
                         </a>
+                        <div class="dropdown-divider"></div>
+                        <div class="container">
+                            <a class="btn btn-primary col-12" href="login.php">
+                                <i class="fas fa-sign-out-alt"></i>
+                                Logout
+                            </a>
+                        </div>
                     </div>
-
-                </div>
-            </li>
-
-        </ul>
-    </div>
-</nav>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
 <section class="konten" style="margin-top:75px;">
     <div class="container">
